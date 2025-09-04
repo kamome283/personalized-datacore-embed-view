a2ef19d8799d2f2d180be891f84c3712b005ffb5
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-09-04 10:19:30 +0900

    feat: タイムスタンプをクリックすると埋め込み先のファイルに移動する機能を実装

--- src/api/personalized-embed.jsx
+++ src/api/personalized-embed.jsx
@@ -11,12 +11,22 @@ function PersonalizedPageEmbed(element) {
     if (!element.value("created")) {
         throw new Error("Does not have created property");
     }
+
+    const path = element.$file;
     const { start, end } = element.$position;
     const created = element.value("created");
+
+    const workspace = dc.app.workspace;
+    if (!workspace) throw new Error("No workspace found");
+    const onTimestampClick = dc.useCallback(
+        (event) => workspace.openLinkText(path, path, event.shiftKey),
+        [path, workspace]
+    );
+
     return (
         <div className="personalized-embed">
-            <h2>{created.toFormat("HH:mm:ss")}</h2>
-            <dc.SpanEmbed path={element.$file} start={start} end={end} showExplain={false} />
+            <h2 onClick={onTimestampClick}>{created.toFormat("HH:mm:ss")}</h2>
+            <dc.SpanEmbed path={path} start={start} end={end} showExplain={false} />
         </div>
     );
 }
