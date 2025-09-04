8803a39dece6bf13b04fa6d40d6049c7eb3e9ceb
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-09-04 15:27:08 +0900

    feat: ページのタイプを選択するラジオボタンの構造を作成

--- src/api/personalized-embed.jsx
+++ src/api/personalized-embed.jsx
@@ -23,9 +23,32 @@ function PersonalizedPageEmbed(element) {
         [path, workspace]
     );
 
+    const inputName = dc.useMemo(() => {
+        return `page-type-${self.crypto.randomUUID()}`;
+    }, []);
+
     return (
         <div className="personalized-embed">
-            <h2 onClick={onTimestampClick}>{created.toFormat("HH:mm:ss")}</h2>
+            <div className="personalized-embed-header">
+                <h2 onClick={onTimestampClick}>{created.toFormat("HH:mm:ss")}</h2>
+                <fieldset>
+                    <input type="radio" id="choice-tweet" name={inputName} value="tweet" />
+                    <label for="choice-tweet">🕊</label>
+
+                    <input type="radio" id="choice-now" name={inputName} value="now" />
+                    <label for="choice-now">🎯</label>
+
+                    <input type="radio" id="choice-task" name={inputName} value="task" />
+                    <label for="choice-task">📋</label>
+
+                    <input type="radio" id="choice-later" name={inputName} value="later" />
+                    <label for="choice-later">⌛</label>
+
+                    <input type="radio" id="choice-done" name={inputName} value="done" checked={true} />
+                    <label for="choice-done">✅</label>
+                </fieldset>
+            </div>
+
             <dc.SpanEmbed path={path} start={start} end={end} showExplain={false} />
         </div>
     );
