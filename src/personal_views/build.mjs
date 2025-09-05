4b64a7b9bd883365b73bdf8a262b73f77aa58233
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-09-05 13:20:27 +0900

    change: ビルド関数をIIFEとフッターの追加を用いた信頼性の高い方式に変更
    信頼性という利点と可読性やパフォーマンスなどの懸念点についての考えは
    https://github.com/kamome283/personalized-datacore-embed-view/pull/3#discussion_r2324028527
    を参照

--- src/personal_views/build.mjs
+++ src/personal_views/build.mjs
@@ -1,5 +1,5 @@
 import * as esbuild from "esbuild";
-import { readdir, readFile, unlink, writeFile } from "fs/promises";
+import { readdir } from "fs/promises";
 import path from "path";
 
 const srcDir = "src/personal_views";
@@ -14,32 +14,17 @@ async function listBuildTargets(srcDir) {
 
 const buildTargets = await listBuildTargets(srcDir);
 
-// Obsidian Datacoreのビューの形式はESMではないため、
-// 一時ファイルにESMとして出力した後に正規表現を用いて編集したものを最終出力とすることで
-// 目的のフォーマットにしている
 async function build(entryPoint, outFile) {
-    const tmpFile = `${outFile}.tmp`;
-
-    // ESBuildを用いてESMとして一時ファイルにビルド
     await esbuild.build({
         entryPoints: [entryPoint],
-        outfile: tmpFile,
+        outfile: outFile,
         bundle: true,
         minify: false,
         jsx: "preserve",
-        format: "esm",
+        format: "iife",
+        globalName: "tempModule",
+        footer: { js: "return tempModule.View;" },
     });
-
-    try {
-        // Obsidian Datacore のビューの形式に編集
-        let content = await readFile(tmpFile, "utf-8");
-        content = content.replace(/function\s+View/, "return function View");
-        content = content.replace(/export\s*\{[^}]+};/s, "");
-        await writeFile(outFile, content, "utf-8");
-    } finally {
-        // 一時ファイルの削除
-        await unlink(tmpFile);
-    }
 }
 
 for (const target of buildTargets) {
